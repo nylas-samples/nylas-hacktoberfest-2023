ce465da3f69d52c87897743272b9453ead2d28f5
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2023-10-13 22:35:36 +0530

    Update calendar.py

--- code-samples/calendar/python/calendar.py
+++ code-samples/calendar/python/calendar.py
@@ -1,17 +1,27 @@
-from nylas import APIClient
+import os
+from nylas import Client
+from dotenv import load_dotenv
 
-client = APIClient(
-    "YOUR_CLIENT_ID",
-    "YOUR_CLIENT_SECRET",
-    access_token="ACCESS_TOKEN"
+load_dotenv()
+
+nylas = Client(
+    api_key = os.getenv("API_KEY")
 )
 
+#creating a calendar
 calendar = {
     "name": "New Calendar Name"
 }
 
+#printing calendars
+no_calendar= 0
+print("SNo. | Calendar Name | Calendar ID\n")
+for i in calendar:
+    print(f"{no_calendar}. {i.name} | {i.id}")
+    no_calendar+=1
+
 try:
-    new_calendar = client.calendars.create(calendar)
+    new_calendar = nylas.calendars.create(calendar)
     print("Calendar created successfully.")
     print("Calendar ID:", new_calendar["id"])
 except Exception as e:
