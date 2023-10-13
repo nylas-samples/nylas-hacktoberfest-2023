1fb016c9213f0083bc052f287d3e87b3fcbe2911
aakzsh <aakashferrari@gmail.com>
2023-10-13 18:04:40 +0530

    add code to handle different 'when' parameters

--- code-samples/calendar-event/python/read_calendar_events/read_events.py
+++ code-samples/calendar-event/python/read_calendar_events/read_events.py
@@ -1,6 +1,7 @@
 import os
 from nylas import Client
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -9,7 +10,6 @@ nylas = Client(
     api_key = os.getenv("API_KEY")
 )  
 
- 
 # get the list of calendars
 response = nylas.calendars.list(identifier=os.getenv("GRANT_ID"))
 calendars = response.data 
@@ -30,16 +30,26 @@ if(selected_calendar<0 or selected_calendar>=len(calendars)):
     raise Exception("Wrong index entered")
 
 
+
+# formats and returns the when parameter to fit in different kinds of cases
+def formatter(when):
+    if when.object=="date":
+        return f"{when.date} all day"
+    elif when.object=="timespan":
+        return f"{datetime.fromtimestamp(when.start_time)} {when.start_timezone} to {datetime.fromtimestamp(when.end_time)} {when.end_timezone}"
+    elif when.object=="datespan":
+        return f"{when.start_date} to {when.end_date}"
+    return ""
+
 # print events of that calendar
 try:
     events = nylas.events.list(identifier=os.getenv("GRANT_ID"), query_params={"calendar_id": calendars[selected_calendar].id}).data
     if(len(events)==0):
         print("No events to show")
     else:
-        print("SNo. Event Title | When | Event ID")
-        event_index = 0
+        event_index = 1
         for event in events:
-            print(f"{event_index}. {event.title} | {event.when.date} | {event.id}")
+            print(f"{event_index}. {event.title}\nEvent ID: {event.id}\nWhen: {formatter(event.when)}\n")
             event_index+=1
 
 except:
